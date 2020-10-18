e85e083a04a6148c9e1c38864602501d85bf09a8
Thomas Lock <t.lock@freshplay.co.uk>
2020-10-18 10:51:03 +0100

    improve login screen

--- scripts/phases/login/background.lua
+++ scripts/phases/login/background.lua
@@ -15,19 +15,29 @@ function initLoginBackground()
     cloudImg = love.graphics.newImage("assets/ui/login/cloud.png")
     mountainImg = love.graphics.newImage("assets/ui/login/mountain.png")
     loginTreeImg = love.graphics.newImage("assets/ui/login/tree.png")
-    love.graphics.setBackgroundColor(0.3,0.3,1)
+    monsterImgs ={
+        love.graphics.newImage("assets/ui/login/monsters/1.png"),
+        love.graphics.newImage("assets/ui/login/monsters/2.png"),
+        love.graphics.newImage("assets/ui/login/monsters/3.png"),
+        love.graphics.newImage("assets/ui/login/monsters/4.png"),
+        love.graphics.newImage("assets/ui/login/monsters/5.png"),
+        love.graphics.newImage("assets/ui/login/monsters/6.png"),
+        love.graphics.newImage("assets/ui/login/monsters/7.png"),
+        love.graphics.newImage("assets/ui/login/monsters/8.png")
+    }
+    love.graphics.setBackgroundColor(0.137,0.537,1)
 
     for i=1,8 do
         clouds[#clouds+1] = {
             x=love.math.random(-love.graphics.getWidth(),love.graphics.getWidth()),
             y=love.math.random(1,love.graphics.getHeight()),
-            speed=love.math.random(4,16)
+            speed=love.math.random(4,8)
         }
     end
     for i = 0,10 do
         loginTrees[i] = {
             x=i*(love.graphics.getWidth()/9),
-            y=love.math.random(-64,-80)
+            y=love.math.random(-80,-110)
         }
     end
     for i = 0,(love.graphics.getWidth()/32)+1 do
@@ -46,9 +56,18 @@ function initLoginBackground()
             y=love.graphics.getHeight()-180
         }
     end
+
+    randoMon1 = love.math.random(1,#monsterImgs)
+    randoMon2 = love.math.random(1,#monsterImgs)
+    randoMonAlpha = 0
+    randoMonAlphaV = 0.02
 end
 
 function drawLoginBackground() 
+    love.graphics.setColor(1,1,1,randoMonAlpha)
+    love.graphics.draw(monsterImgs[randoMon1],love.graphics.getWidth()/2-512,love.graphics.getHeight()-360)
+    love.graphics.draw(monsterImgs[randoMon2],love.graphics.getWidth()/2+256,love.graphics.getHeight()-360)
+    love.graphics.setColor(1,1,1,1)
     for i,v in pairs(clouds) do
         love.graphics.draw(cloudImg, v.x, v.y)
     end
@@ -66,7 +85,7 @@ end
 
 function updateLoginBackground(dt) 
     for i,v in pairs(clouds) do
-        v.x = v.x + v.speed*dt
+        v.x = v.x - v.speed*dt
         if v.x > love.graphics.getWidth()+cloudImg:getWidth() then
             v.x = -cloudImg:getWidth()
         end
@@ -80,14 +99,23 @@ function updateLoginBackground(dt)
     for i,v in pairs(loginGrass) do
         v.x = v.x - 64*dt
         if v.x <= -32 then
-            v.x = love.graphics.getWidth()+32
+            v.x = love.graphics.getWidth()
         end
     end
     for i, v in pairs(loginMountains) do
-        v.x = v.x - 32*dt
+        v.x = v.x - 24*dt
         if v.x <= -128 then
             v.x = love.graphics.getWidth()+115
         end
+    end
+
+    randoMonAlpha = randoMonAlpha + randoMonAlphaV*dt
+    if randoMonAlpha > 0.1 then
+        randoMonAlphaV = -0.02
+    elseif randoMonAlpha < 0 then
+        randoMon1 = love.math.random(1,#monsterImgs)
+        randoMon2 = love.math.random(1,#monsterImgs)
+        randoMonAlphaV = 0.02
     end
 
     loginPlayerX = loginPlayerX + 32*dt
