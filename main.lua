960ed0649b94461e91072b29bf2b1db9a9ff479e
danjoemybro <danjoestubbs@gmail.com>
2020-10-29 16:26:51 +0000

    Chat scrolling

--- main.lua
+++ main.lua
@@ -110,7 +110,7 @@ function love.draw()
     end
 
     love.graphics.setColor(1,1,1,1)
-    local mx, my = love.mouse.getPosition()
+    mx, my = love.mouse.getPosition()
 	love.graphics.draw(mouseImg, mx, my)
 
 
