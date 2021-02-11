d18637fa9ea83ea3ff2af5fd93ec2eb905eed070
danjoemybro <danjoestubbs@gmail.com>
2021-02-11 10:00:54 +0000

    not working

--- scripts/player/targeting.lua
+++ scripts/player/targeting.lua
@@ -6,94 +6,72 @@ function initTargeting()
         left = false,
         right = false
     }
+
+    targetHeld = false
+    keys = {keybinds.ATTACK_UP, keybinds.ATTACK_DOWN, keybinds.ATTACK_LEFT, keybinds.ATTACK_RIGHT}
+    heldKeys = {false, false, false, false,}
+    targetKeys = {false, false, false, false,}
 end
 
-local targetHeld = false
-
 function checkTargetingPress(key)
-    -- if key == keybinds.ATTACK_UP then
-    --     player.target.active = true
-    --     if player.target.y == player.y - 1 then
-    --         player.target.y = player.y
-    --         targetActive.up = false
-    --     else
-    --         player.target.y = player.y - 1
-    --         player.target.x = player.x
-    --         targetActive.left = false
-    --         targetActive.right = false
-    --         targetActive.up = true
-    --         targetActive.down = false
-    --     end
-    -- elseif key == keybinds.ATTACK_DOWN then
-    --     player.target.active = true
-    --     if player.target.y == player.y + 1 then
-    --         player.target.y = player.y
-    --         targetActive.down = false
-    --     else
-    --         player.target.y = player.y + 1
-    --         player.target.x = player.x
-    --         targetActive.left = false
-    --         targetActive.right = false
-    --         targetActive.up = false
-    --         targetActive.down = true
-    --     end
-    -- end
-    -- if key == keybinds.ATTACK_LEFT then
-    --     player.target.active = true
-    --     if player.target.x == player.x - 1 then
-    --         player.target.x = player.x
-    --         targetActive.left = false
-    --     else
-    --         player.target.x = player.x - 1
-    --         player.target.y = player.y
-    --         targetActive.left = true
-    --         targetActive.right = false
-    --         targetActive.up = false
-    --         targetActive.down = false
-    --     end
-    -- elseif key == keybinds.ATTACK_RIGHT then
-    --     player.target.active = true
-    --     if player.target.x == player.x + 1 then
-    --         player.target.x = player.x
-    --         targetActive.right = false
-    --     else
-    --         player.target.x = player.x + 1
-    --         player.target.y = player.y
-    --         targetActive.left = false
-    --         targetActive.right = true
-    --         targetActive.up = false
-    --         targetActive.down = false
-    --     end
-    -- end
+
+    if targetHeld then
+        for i,v in ipairs(keys) do
+            if key == v then
+                if heldKeys[i] == false then
+                    heldKeys = {false, false, false, false,}
+                    targetKeys = {false, false, false, false,}
+                end
+            end
+        end
+    end
+    for i,v in ipairs(keys) do
+        if key == v then
+            targetKeys[i] = true
+        end
+    end
+    print("Press " .. json:encode(targetKeys))
 end
 
 function checkTargetingRelease(key)
-  
+    if not love.keyboard.isDown("lshift") then
+        targetHeld = true
+        for i,v in ipairs(keys) do
+            if key == v then
+                targetKeys[i] = false
+            end
+        end
+    else
+        targetHeld = true
+        heldKeys = copy(targetKeys)
+    end
+    print("Release " .. json:encode(targetKeys))
+    -- checkTargeting()
 end
 
 function checkTargeting() -- Check which keys are down and place the player target accordingly
     local wasActive = player.target.active
     player.target = {x = player.x, y = player.y, active = false}
 
-    if isMouseDown() then
-        checkMouseTargeting()
-    else
-        if love.keyboard.isDown(keybinds.ATTACK_UP) then
+    -- if isMouseDown() then
+    --     checkMouseTargeting()
+    -- else
+        if targetKeys[1] then
             player.target.active = true
             player.target.y = player.y - 1
-        elseif love.keyboard.isDown(keybinds.ATTACK_DOWN) then
+        elseif targetKeys[2] then
             player.target.active = true
             player.target.y = player.y + 1
         end
 
-        if love.keyboard.isDown(keybinds.ATTACK_LEFT) then
+        if targetKeys[3] then
             player.target.active = true
             player.target.x = player.x - 1
-        elseif love.keyboard.isDown(keybinds.ATTACK_RIGHT) then
+        elseif targetKeys[4] then
             player.target.active = true
             player.target.x = player.x + 1
         end
-    end
+    -- end
 
     if not wasActive and player.target.active then
         -- newly active, trigger a send to make things feel a tad more responsive
