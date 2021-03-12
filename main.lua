c5fb7533e2468d1260ba95ed4968eca8b5ede9be
Thomas Lock <t.lock@freshplay.co.uk>
2021-03-12 17:11:26 +0000

    1.2.1

--- main.lua
+++ main.lua
@@ -62,7 +62,7 @@ utf8 = require("utf8")
 newOutliner = require 'scripts.libraries.outliner'
 
 version = "Early Access" 
-versionType = "dev" -- "dev" for quick login, "release" for not
+versionType = "release" -- "dev" for quick login, "release" for not
 if versionType == "dev" then require 'dev' end
 versionNumber = "1.2.1" -- very important for settings
 
