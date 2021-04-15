bc149ee1760fe2c57c1075538f7997e622e8199f
danjoemybro <danjoestubbs@gmail.com>
2021-04-15 11:47:04 +0100

    world animations lag off

--- scripts/world/world.lua
+++ scripts/world/world.lua
@@ -42,15 +42,17 @@ function createWorld()
         if orCalc(key, tab) then
             for i,v in ipairs(tiles) do
 
-                worldLookup[v.X..","..v.Y] = copy(v)
-                -- addWorldEmitter(v)
+                worldLookup[v.X..","..v.Y] = v
                 
-                if not isTileType(v.ForegroundTile, "Dead") and isTileType(v.ForegroundTile, "Tree") and love.math.random(1,5) == 1 then
-                    if isTileType(v.ForegroundTile, "Snowy") then addLeaf(v.X*32 + 16, v.Y*32 + 16, "snowy tree")
-                    else addLeaf(v.X*32 + 16, v.Y*32 + 16, "tree") end
-                elseif isTileType(v.ForegroundTile, "Campfire") then addLeaf(v.X*32 + 16, v.Y*32 + 8, "fire")
-                elseif isTileType(v.ForegroundTile, "Sand") then -- addLeaf(v.X*32 + 16, v.Y*32 + 16, "sand")
-                elseif isTileType(v.GroundTile, "Murky") then addLeaf(v.X*32, v.Y*32+16, "murky") end
+                if showWorldAnimations then
+                    addWorldEmitter(v)
+                    if not isTileType(v.ForegroundTile, "Dead") and isTileType(v.ForegroundTile, "Tree") and love.math.random(1,5) == 1 then
+                        if isTileType(v.ForegroundTile, "Snowy") then addLeaf(v.X*32 + 16, v.Y*32 + 16, "snowy tree")
+                        else addLeaf(v.X*32 + 16, v.Y*32 + 16, "tree") end
+                    elseif isTileType(v.ForegroundTile, "Campfire") then addLeaf(v.X*32 + 16, v.Y*32 + 8, "fire")
+                    elseif isTileType(v.ForegroundTile, "Sand") then -- addLeaf(v.X*32 + 16, v.Y*32 + 16, "sand")
+                    elseif isTileType(v.GroundTile, "Murky") then addLeaf(v.X*32, v.Y*32+16, "murky") end
+                end
     
                 if lightGivers[v.ForegroundTile] and not lightSource[v.X .. "," .. v.Y] then
                     lightSource[v.X .. "," .. v.Y] = true
