0db7e691a5227364eafb46cca0e8876370b7c793
danjoemybro <danjoestubbs@gmail.com>
2021-04-15 17:45:19 +0100

    shields

--- scripts/player/animation.lua
+++ scripts/player/animation.lua
@@ -14,7 +14,7 @@ function initAnimation()
     animatePlayerAttack = false
 end
 
-local walkSpeed, attackSpeed = 6, 10
+local walkSpeed, attackSpeed = 8, 10
 local idle, walkStart, walkEnd = 1, 2, 5
 
 function animateCharacter(dt, bool)
@@ -53,7 +53,7 @@ function animateOtherPlayer(dt, bool, i)
             plr.Frame = 10
             plr.frameAmount = 0
         end
-        plr.frameAmount = plr.frameAmount + speed * dt
+        plr.frameAmount = plr.frameAmount + attackSpeed * dt
         if plr.frameAmount >= 1 then
             plr.Frame = (plr.Frame or 10) + 1
             if plr.Frame >= 16 then
@@ -64,12 +64,12 @@ function animateOtherPlayer(dt, bool, i)
         end
     elseif bool or (plr.Frame and plr.Frame > 1) then
         if not plr.frameAmount then plr.frameAmount = 0 end
-        plr.frameAmount = plr.frameAmount + speed * dt
+        plr.frameAmount = plr.frameAmount + walkSpeed * dt
         if plr.frameAmount >= 1 then
             plr.Frame = (plr.Frame or 1) + 1
-            if not bool then plr.Frame = 1 end
-            if plr.Frame > 5 then plr.Frame = 1 end
+            if plr.Frame > walkEnd then plr.Frame = walkStart end
             plr.frameAmount = 0
+            if not bool then plr.Frame = idle end
         end
     end
 end
@@ -83,7 +83,25 @@ end
 function drawAnimation(v,x,y,dir)
     y = y - 3
     frame = v.Frame or 1
+    local off = 0
+    if frame == 1 or frame % 2 == 0 then off = 1 end
     love.graphics.draw(baseShadow, x, y + 15, 0, dir, 1)
+
+
+    drawWeapon(v,x,y,dir,frame)
+
+
+    love.graphics.setColor(1,1,1)
+    love.graphics.draw(baseSpriteSheet, baseImages[frame], x, y, 0, dir, 1)
+
+    love.graphics.setBlendMode("add")
+    if frame >= 12 and frame <= 14 then love.graphics.draw(baseSwing, swingImages[frame - 11], x - (27 * dir), y - 27, 0, dir, 1) end
+    love.graphics.setBlendMode("alpha")
+
+    love.graphics.print(frame, x, y+34)
+end
+
+function drawWeapon(v,x,y,dir,frame)
     if frame >= 10 then
         local wx,wy = x - (getImgIfNotExist(v.Weapon.ImgPath):getWidth() - 32) * dir, y - (getImgIfNotExist(v.Weapon.ImgPath):getHeight() - 32)
         if frame == 10 then drawAnimationWeapon(v,wx,wy - 3, dir)
@@ -93,21 +111,16 @@ function drawAnimation(v,x,y,dir)
     else
         local off = 0
         if frame == 1 or frame % 2 == 0 then off = 1 end
+
+        local shieldOff = 4
+        if frame == 3 or frame == 5 then shieldOff = 5 elseif frame == 4 then shieldOff = 6 end
+        if v.ShieldID ~= 0 then drawArmourImage(x - shieldOff * dir, y - off + 1, v, ad, "ShieldFalse", dir) end
         local wx, wy, r = x,y - off, 0
         if string.find(v.Weapon.ImgPath, "a1/special", 18) or
             string.find(v.Weapon.ImgPath, "sword", 22) or
             string.find(v.Weapon.ImgPath, "dagger", 22) then wx, wy, r = wx + 32 * dir, wy + 32, 180 end
         drawAnimationWeapon(v, wx, wy, dir, r)
     end
-
-    love.graphics.setColor(1,1,1)
-    love.graphics.draw(baseSpriteSheet, baseImages[frame], x, y, 0, dir, 1)
-
-    love.graphics.setBlendMode("add")
-    if frame >= 12 and frame <= 14 then love.graphics.draw(baseSwing, swingImages[frame - 11], x - (27 * dir), y - 27, 0, dir, 1) end
-    love.graphics.setBlendMode("alpha")
-
-    -- love.graphics.print(frame, x, y+34)
 end
 
 function drawAnimationWeapon(v,x,y,dir,r)
--- scripts/player/other_players.lua
+++ scripts/player/other_players.lua
@@ -43,7 +43,7 @@ function drawCharacter(v, x, y, ad)
             love.graphics.setColor(1,1,1)
             drawBuddy(v)
             if v.RedAlpha then love.graphics.setColor(1, 1-v.RedAlpha, 1-v.RedAlpha) end
-            if v.ShieldID ~= 0 then drawArmourImage(x + offsetX,y,v,ad,"ShieldFalse",direction) end
+            -- if v.ShieldID ~= 0 then drawArmourImage(x + offsetX  - 5 * direction,y,v,ad,"ShieldFalse",direction) end
             -- drawWeapon(x,y,v,ad,direction,offsetX)
             if v.Invulnerability >= 0 then
                 love.graphics.setColor(1,1,1,0.3)
@@ -60,7 +60,7 @@ function drawCharacter(v, x, y, ad)
             -- drawArmourImage(x,y,v,ad,"HeadArmour")
             love.graphics.setColor(1,1,1)
             drawMount(x,y,v,ad,direction,mountOffsetX,notBoat,"/fore.png")
-            if v.IsShield and v.ShieldID ~= 0 and notBoat then drawArmourImage(x+12,y,v,ad,"Shield",direction) end
+            -- if v.IsShield and v.ShieldID ~= 0 and notBoat then drawArmourImage(x+12 - 5 * direction,y,v,ad,"Shield",direction) end
         end
 
         -- drawRangedWeaponEffects(v, x, y)
@@ -86,31 +86,31 @@ function drawMount(x,y,v,ad,direction,mountOffsetX,notBoat,type)
     end
 end
 
-function drawWeapon(x,y,v,ad,direction,offsetX)
+-- function drawWeapon(x,y,v,ad,direction,offsetX)
 
-    if not itemImg[v.Weapon.ImgPath] then
-        if love.filesystem.getInfo(v.Weapon.ImgPath) then
-            itemImg[v.Weapon.ImgPath] = love.graphics.newImage(v.Weapon.ImgPath)
-        else
-            itemImg[v.Weapon.ImgPath] = love.graphics.newImage("assets/error.png")
-        end
-    end
+--     if not itemImg[v.Weapon.ImgPath] then
+--         if love.filesystem.getInfo(v.Weapon.ImgPath) then
+--             itemImg[v.Weapon.ImgPath] = love.graphics.newImage(v.Weapon.ImgPath)
+--         else
+--             itemImg[v.Weapon.ImgPath] = love.graphics.newImage("assets/error.png")
+--         end
+--     end
 
-    if v["WeaponID"] ~= 0 then
-        if v.RedAlpha then love.graphics.setColor(1, 1-v.RedAlpha, 1-v.RedAlpha) else love.graphics.setColor(1, 1, 1) end
-        love.graphics.draw(itemImg[v.Weapon.ImgPath], x - (itemImg[v.Weapon.ImgPath]:getWidth() - 32) * direction + offsetX, y - (itemImg[v.Weapon.ImgPath]:getHeight() - 32), 0, direction, 1, 0, 0)
-        if v.Weapon.Enchantment ~= "None" then
-            love.graphics.push()
-                love.graphics.stencil(function() 
-                    love.graphics.setShader(alphaShader)
-                    love.graphics.draw(itemImg[v.Weapon.ImgPath], x - (itemImg[v.Weapon.ImgPath]:getWidth() - 32) * direction + offsetX, y - (itemImg[v.Weapon.ImgPath]:getHeight() - 32), 0, direction, 1, 0, 0)
-                    love.graphics.setShader()
-                end)
-                drawEnchantment(x - (itemImg[v.Weapon.ImgPath]:getWidth() - 32) + offsetX, y)
-            love.graphics.pop()
-        end
-    end
-end
+--     if v["WeaponID"] ~= 0 then
+--         if v.RedAlpha then love.graphics.setColor(1, 1-v.RedAlpha, 1-v.RedAlpha) else love.graphics.setColor(1, 1, 1) end
+--         love.graphics.draw(itemImg[v.Weapon.ImgPath], x - (itemImg[v.Weapon.ImgPath]:getWidth() - 32) * direction + offsetX, y - (itemImg[v.Weapon.ImgPath]:getHeight() - 32), 0, direction, 1, 0, 0)
+--         if v.Weapon.Enchantment ~= "None" then
+--             love.graphics.push()
+--                 love.graphics.stencil(function() 
+--                     love.graphics.setShader(alphaShader)
+--                     love.graphics.draw(itemImg[v.Weapon.ImgPath], x - (itemImg[v.Weapon.ImgPath]:getWidth() - 32) * direction + offsetX, y - (itemImg[v.Weapon.ImgPath]:getHeight() - 32), 0, direction, 1, 0, 0)
+--                     love.graphics.setShader()
+--                 end)
+--                 drawEnchantment(x - (itemImg[v.Weapon.ImgPath]:getWidth() - 32) + offsetX, y)
+--             love.graphics.pop()
+--         end
+--     end
+-- end
 
 function drawArmourImage(x,y,v,ad,type,direction)
     if v[type.."ID"] ~= 0 then
